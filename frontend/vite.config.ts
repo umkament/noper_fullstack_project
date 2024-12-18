58746d3464b1a286bd7a176e328d3913d3c2cfbc
umkament <97944243+umkament@users.noreply.github.com>
2024-12-19 01:22:09 +0700

    fix ~

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,4 +14,8 @@ export default defineConfig({
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
+  server: {
+    host: '0.0.0.0', // Ожидание подключения на всех интерфейсах
+    port: 5173, // Порт, на котором приложение будет работать
+  },
 })
