cf106a207a4972e391c020e6de9bb2556d1d5e2c
umkament <97944243+umkament@users.noreply.github.com>
2024-12-19 00:16:19 +0700

    fix port

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,11 +7,15 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   base: '/noper_app_front/',
   build: {
+    chunkSizeWarningLimit: 3000,
     outDir: 'dist',
-    chunkSizeWarningLimit: 3000, 
   },
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
+  server: {
+    host: '0.0.0.0', // Это нужно для того, чтобы сервер Vite был доступен из контейнеров Docker
+    port: 3000, // Убедитесь, что порт совпадает с тем, что указан в конфигурации Nginx
   },
 })
